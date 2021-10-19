dda01b8a56c83707e141d0ad6a4ce8214646bf33
Gustavo <82328081+GustavoLuisdO@users.noreply.github.com>
2021-10-19 11:58:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function tabuada(){
+  var num = document.getElementById('num').value
+  var resultado = document.getElementById('resultado')
+
+  if(num === ""){
+    resultado.innerHTML = 'Insira um número'
+  }
+  else{
+    for(var cont=1; cont<=10; cont++){
+      resultado.innerHTML += `${num} x ${cont} = ${(num*cont)} <br>`
+    }
+  }
+}
